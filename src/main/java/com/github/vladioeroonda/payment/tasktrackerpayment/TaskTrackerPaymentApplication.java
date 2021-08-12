165a9f8f6501663ce1b0543fa524f50f11650894
VladioEroonda <vladioaf8@gmail.com>
2021-08-12 19:35:37 +0300

    Initial commit

---
+++ src/main/java/com/github/vladioeroonda/payment/tasktrackerpayment/TaskTrackerPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.github.vladioeroonda.payment.tasktrackerpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/vladioeroonda/payment/tasktrackerpayment/TaskTrackerPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vladioeroonda.payment.tasktrackerpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
